ab944eed72c322b6e726b1aff6fa6ced1093ee97
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-29 10:09:05 -0500

    building sidebar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Sidebar from './Sidebar'
+import {Container,Col,Row} from 'react-bootstrap'
+
+
+
+const items = [
+    { name: 'home', label: 'Home' },
+    { name: 'billing', label: 'Billing' },
+    { name: 'settings', label: 'Settings' },
+  ]
+  
+  
 
 export class Home extends Component {
     render() {
         return (
-            <div>
-                Hello
-            </div>
+ <Container fluid >
+  <Row>
+    <Col><Sidebar items={items} /></Col>
+    <Col xs={7}>2 of 3 (wider)</Col>
+    <Col>3 of 3</Col>
+  </Row>
+  
+</Container>
         )
     }
 }
