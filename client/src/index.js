9860adbd2d69abae2413855d49f716c2237b2c4b
Meryem Kömür <meryemkmr36@gmail.com>
2020-04-30 13:08:28 -0500

    reverted some changes.  still don't know how index page is working -veronic

--- client/src/index.js
+++ client/src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-// import { Provider } from 'react-redux';
-// import store from './store'
-// import { BrowserRouter, Switch, Route } from 'react-router-dom'
-// import BaseLayout from './components/layout/BaseLayout'
-// import Home from './components/Home'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import BaseLayout from './componenets/layout/BaseLayout'
 
 
 
 ReactDOM.render(
-  
-    // <BrowserRouter>
-    // <BaseLayout>
-    // <Switch>
-    // <Route exact path="/" component={Home} />
-    // <Route  path="/cart" component={App} />
-    //   </Switch>
-    //   </BaseLayout>
-    //   </BrowserRouter>
-    <App /> 
+  // <Provider store={store}>
+  <BrowserRouter>
+      <BaseLayout>
+       
+        <Switch>
+        
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cart" component={Cart} />
+         
+        </Switch>
+      </BaseLayout>
+    </BrowserRouter>
+    // </Provider>
     ,
   document.getElementById('root')
 );
+
 
